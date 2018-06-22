1ef76efd81d18fa61736e75626576017ee0ca774
Paul <paulmlee7@gmail.com>
2018-06-22 22:16:01 +0000

    Add edit, update, show and index listing.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,67 @@
 # camel case, plural, uppercased
 class ArticlesController < ApplicationController
   
+  def index
+    #instance variable can be anything. 'articles' in easier right now. use this variable in embedded ruby in html.erb files
+    @articles = Article.all
+  end
+  
+  
+  
   def new
     @article = Article.new
   end
   
+  
+  
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  
+  
+  
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params) # creates the new article and saves
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+      
+    #goes into else if the validations have failed.  
+    else
+      # refreshes page with validation errors essentially
+      render 'new'
+    end
   end
   
-  # defining articles params here
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "article updated!"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
+  
+  #renders show.html, able to use a specific article object in it.
+  def show
+    #article instance variable
+    @article = Article.find(params[:id])
+  end
+  
+  
+  
+  # defining articles params here. whitelisting
   private
   def article_params
+    # from params hash, allow title and description.
     params.require(:article).permit(:title, :description)
   end
+  
+  
 end
