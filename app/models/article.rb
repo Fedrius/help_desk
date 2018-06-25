d31a55b0d3b1636c3bf1048624cf501917d301bb
Paul <paulmlee7@gmail.com>
2018-06-25 23:58:35 +0000

    added Users to db and created an assoication

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,12 @@
 # class Article <ApplicationRecord (rails 5)
 class Article < ActiveRecord::Base
+  # many articles belong to one user
+  belongs_to :user
+  
   # validates to make sure theres an input in record and certain length
   validates :title, presence: true, length: {minimum: 3, maximum: 50 }
   validates :description, presence: true, length: {minimum: 5, maximum: 300 }
+  
+  # ensure userid is present..
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
+  # one user has many articles
+  has_many :articles
+  
+  #before user hits db, email will be downcased
+  before_save {self.email = email.downcase }
+  
   validates :username, presence: true, 
             uniqueness: { case_sensitive: false }, 
             length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 105 },
             uniqueness: { case_sensitive: false },
-            format: { with VALID_EMAIL_REGEX }
+            format: { with: VALID_EMAIL_REGEX }
 end
