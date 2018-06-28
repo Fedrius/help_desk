40e174fc72d1d4befe9c3f01d65de5314e044942
Paul <paulmlee7@gmail.com>
2018-06-28 00:06:59 +0000

    added pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   
+  def index
+    @users = User.paginate(page: params[:page], per_page: 1)
+  end
+  
   def new
     @user = User.new
   end
@@ -33,6 +37,12 @@ class UsersController < ApplicationController
     end
   end
   
+  
+  def show
+    @user = User.find(params[:id])
+    
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 1)
+  end
   
   
   # whitelist the input
