22c4ddd1d2b1941512958177383cef94abd5c2d8
Paul <paulmlee7@gmail.com>
2018-07-03 21:16:59 +0000

    Added view buttons to help desk tickets.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,7 @@ class Ticket < ActiveRecord::Base
   
   validates :title, presence: true, length: {minimum: 5, maximum: 50 }
   validates :description, presence: true, length: {minimum: 5, maximum: 500 }
+  
+  # ensure userid is present..
+  validates :user_id, presence: true
 end
