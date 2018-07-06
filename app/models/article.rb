5f7b787fd278cd02f4d6e3c5b3863ce33eac90fa
Paul <paulmlee7@gmail.com>
2018-07-06 21:54:56 +0000

    Added a categories table to DB with a many to many association to articles.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,10 @@ class Article < ActiveRecord::Base
   # many articles belong to one user
   belongs_to :user
   
+  # many..
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  
   # validates to make sure theres an input in record and certain length
   validates :title, presence: true, length: {minimum: 5, maximum: 50 }
   validates :description, presence: true, length: {minimum: 5, maximum: 500 }
