b30239d673d509c9e000aa01a62857bdac905a53
Paul <paulmlee7@gmail.com>
2018-07-06 00:55:06 +0000

    changed admin route when logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,11 @@ class SessionsController < ApplicationController
       
       flash[:success] = "you have successfully logged in"
       
-      redirect_to user_path(user)
+      if user.admin == true
+        redirect_to test_help_desk_path
+      else
+        redirect_to user_path(user)
+      end
     else
       # using .now because render new is not a new request
       flash.now[:danger] = "there was something wrong with your login info"
